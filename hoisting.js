7331a452e10c791bed3fdaa4ea2c56c56b433672
YuSeongChoi <marine8743@naver.com>
2020-04-27 15:28:17 +0900

    Hoisting 2

--- hoisting.js
+++ hoisting.js
@@ -11,3 +11,24 @@ hello2();
 function hello2() {
     console.log('Hello 2');
 }
+
+// var 에서도 가능
+age = 6;
+age++;
+console.log(age);
+
+var age;
+
+// example++
+console.log(name);
+name = 'Mark';
+console.log(name);
+var name = "Zico"; // == var name;
+
+//즉, 선언문만 맨위로 호이스팅된다!!
+
+//let을 사용!!
+console.log(name);
+name = 'Mark';
+console.log(name);
+let name;
